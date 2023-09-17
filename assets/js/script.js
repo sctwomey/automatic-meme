0e10a2e13250c84b634b327d1c3a5a28609aa9a2
sctwomey <sctwomey@gmail.com>
2023-09-16 19:02:31 -0700

    Update js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,41 @@ function displayDayDate() {
   dayDisplayEl.text(todaysDayDate);
 };
 
+// Function that is called once everything renders on the webpage.
+function plannerHourColorSwitcher() {
+  // Tracks the current hour in the day.
+  let rightNow = dayjs().hour();
+
+  $(".time-block").each(function () {
+    var hourBlock = parseInt($(this).attr("id").split("hour")[1] * -1);
+    // This Compares the hours in the hour blocks, and adds the matching classes for the matching background colors.
+    if (hourBlock < rightNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    };
+    if (hourBlock === rightNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    };
+    if (hourBlock > rightNow) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+
+    };
+  });
+};
+
 $(function () {
   // This is the event listener for the save button (saveBtn).
   $(".saveBtn").on("click", function () {
-    var timeText = $(this).siblings(".description").val();
-    var blockTime = $(this).parent().attr("id");
+    var hourText = $(this).siblings(".description").val();
+    var blockHour = $(this).parent().attr("id");
     // This saves the text from the time blocks into the local storage.
-    localStorage.setItem(blockTime, timeText);
+    localStorage.setItem(blockHour, hourText);
   });
-
-  // Function that is called once everything renders on the webpage.
-  function plannerHourColorSwitcher() {
-    // Tracks the current hour in the day.
-    let rightNow = dayjs().hour();
-
-    $(".time-block").each(function () {
-      var hourBlock = parseInt($(this).attr("id").split("hour")[1] * -1);
-      // This Compares the hours in the hour blocks, and adds the matching classes for the matching background colors.
-      if (hourBlock < rightNow) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
-      };
-      if (hourBlock === rightNow) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-      };
-      if (hourBlock > rightNow) {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-
-      };
-    });
-  };
 
   // This retrieves the user input from the local storage, if there is anything to retrieve.
   $("#hour-8 .description").val(localStorage.getItem("hour-8"));
