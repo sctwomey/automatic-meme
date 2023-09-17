fc9ceaa160878fbd9f12ce5f0d3b641282d9f1ee
sctwomey <sctwomey@gmail.com>
2023-09-16 18:23:18 -0700

    Add functions to switch background colors in planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 let timeDisplayEl = $('#currentDay');
 
-// This handles displaying the time
+// This handles displaying the time.
 function displayTime() {
   var todaysDayDate = dayjs().format('dddd, MMMM DD, YYYY');
   timeDisplayEl.text(todaysDayDate);
 };
 
 $(function () {
-
   // This is the event listener for the save button (saveBtn).
   $(".saveBtn").on("click", function () {
     var timeText = $(this).siblings(".description").val();
@@ -20,25 +16,35 @@ $(function () {
     localStorage.setItem(blockTime, timeText);
   });
 
+  // Function that is called once everything renders on the webpage.
+  function plannerHourColorSwitcher() {
+    // Tracks the current hour in the day.
+    let rightNow = dayjs().hour();
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+    $(".time-block").each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("hour")[1] * -1);
+      // This Compares the hours in the hour blocks, and adds the matching classes for the matching background colors.
+      if (hourBlock < rightNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      };
+      if (hourBlock === rightNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      };
+      if (hourBlock > rightNow) {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+      };
+    });
+  };
+
+  plannerHourColorSwitcher();
+
 });
 
 displayTime();
